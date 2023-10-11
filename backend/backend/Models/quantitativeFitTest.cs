1e234f1b356f967cea1b1eaa7350be4d95fa9d96
anthonymorton <anthonymorton760@gmail.com>
2023-10-10 18:28:56 -0700

    working on quantitative fit test models

---
+++ backend/backend/Models/quantitativeFitTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class QuantitativeFitTest
+{
+    [Key]
+    public int quantitativeTestID { get; set; }
+
+    public Boolean testpass { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public System.DateTime testdate { get; set; }
+
+    [Required]
+    [DataType(DataType.Time)]
+    public System.DateTime testtime { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public System.DateTime testexpiration { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor1 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor2 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor3 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor4 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor5 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor6 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor7 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string fitfactor8 { get; set; }
+
+    [Required]
+    [MaxLength(7)]
+    public string overallfitfactor { get; set; }
+
+    [Required]
+    public int employeeID { get; set; }
+
+    [Required]
+    public int respiratorID { get; set; }
+
+}
+
+
+
