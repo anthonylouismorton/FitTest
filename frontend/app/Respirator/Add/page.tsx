71574839f8484524ff0d355e6f99fe39c9b9507d
anthonymorton <anthonymorton760@gmail.com>
2023-10-13 18:10:22 -0700

    finishing up final touches

--- frontend/app/Respirator/Add/page.tsx
+++ frontend/app/Respirator/Add/page.tsx
@@ -55,7 +55,7 @@ const Add = () => {
   return (
     <div className="flex items-center justify-center min-h-screen w-full">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h3 className="text-2xl font-semibold mb-4 text-center">Edit Respirator</h3>
+        <h3 className="text-2xl font-semibold mb-4 text-center">Add Respirator</h3>
         <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-md font-medium text-black">
